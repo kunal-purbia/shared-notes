034d8520b0be22ae32ca7290f2f8289ff30c3049
kunal-purbia <kunalpurbia1998@gmail.com>
2025-03-22 14:59:24 +0530

    share api done

---
+++ src/app/api/notes/shared/[id]/route.ts
@@ -0,0 +1,17 @@
+import { connectDatabase } from "@/utils/db";
+import { NextRequest, NextResponse } from "next/server";
+import Note from "@/models/Notes.Schema";
+
+export async function GET(req: NextRequest, { params }) {
+  await connectDatabase();
+  const note = await Note.findById(params.id);
+  if (!note) {
+    return NextResponse.json({ error: "Note not found" }, { status: 404 });
+  }
+
+  return NextResponse.json({
+    title: note.title,
+    content: note.content,
+    sharedWith: note.sharedWith,
+  });
+}
