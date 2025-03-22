5a636c78ab285a7dd580b1c3be5b2e8e430f1438
kunal-purbia <kunalpurbia1998@gmail.com>
2025-03-22 14:55:19 +0530

    note update delete api done

--- src/app/api/notes/[id]/route.ts
+++ src/app/api/notes/[id]/route.ts
@@ -17,3 +17,42 @@ export async function GET(req: NextRequest, { params }) {
 
   return NextResponse.json(note);
 }
+
+export async function PUT(req: NextRequest, { params }) {
+  await connectDatabase();
+
+  const user: any = await verifyToken(req);
+  if (!user)
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+  const note = await Note.findById(params.id);
+  if (!note || note.userId !== user.id) {
+    return NextResponse.json({ error: "Note not found." }, { status: 404 });
+  }
+
+  const { title, content, sharedWith } = await req.json();
+
+  note.title = title || note.title;
+  note.content = content || note.content;
+  note.sharedWith = sharedWith || note.sharedWith;
+
+  await note.save();
+  return NextResponse.json(note);
+}
+
+export async function DELETE(req: NextRequest, { params }) {
+  await connectDatabase();
+
+  const user: any = await verifyToken(req);
+  if (!user)
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+  const note = await Note.findById(params.id);
+  if (!note || note.userId !== user.id) {
+    return NextResponse.json({ error: "Note not found." }, { status: 404 });
+  }
+
+  await note.deleteOne();
+
+  return NextResponse.json({ message: "Note deleted successfully" });
+}
